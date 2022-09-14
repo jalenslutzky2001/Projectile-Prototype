861f616a6137bafbf25cdb4abd2f172e18cfdac2
cnfarmer99 <cnf245@gmail.com>
2022-09-14 19:53:54 -0400

    Fire action added to movement script

--- Bubble Shooter/Assets/Movement.cs
+++ Bubble Shooter/Assets/Movement.cs
@@ -70,5 +70,8 @@ public class Movement : MonoBehaviour
             playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
             transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
         }
+
+        if (Input.GetButtonDown("Fire1")) 
+            Debug.Log("Fire!");
     }
 }
